4dcfb81bfc538d91b202a97feff35a48cbef4f8f
Martin Maly <maly@maly.cz>
2019-07-28 16:16:50 +0200

    list to question

--- js/language.js
+++ js/language.js
@@ -13,4 +13,16 @@ var listToText = s => {
   return out;
 };
 
-module.exports = { listToText };
+var listToQuestion = s => {
+  //var s = l.map(q => q.names[3]);
+  var out = "";
+  for (var i = 0; i < s.length; i++) {
+    out += s[i];
+    if (i === s.length - 1) out += "?";
+    else if (i === s.length - 2) out += ", nebo ";
+    else out += ", ";
+  }
+  return out;
+};
+
+module.exports = { listToText, listToQuestion };
