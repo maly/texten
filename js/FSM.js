7f25ad585c6db1ab242d3a61201b6d9e7c4acffd
Martin Maly <maly@maly.cz>
2019-07-30 17:15:50 +0200

    Music fadeout

--- js/FSM.js
+++ js/FSM.js
@@ -1,9 +1,8 @@
 const display = require("./display.js");
 
-
-var timer = require("./timer.js")
-var music = require("./music.js")
-var video = require("./video.js")
+var timer = require("./timer.js");
+var music = require("./music.js");
+var video = require("./video.js");
 
 const keyboard = require("./keyboard.js");
 const game = require("./game.js");
@@ -11,93 +10,94 @@ const game = require("./game.js");
 var introTimer;
 
 var FSM = {
-    state: "begin",
-    newState(ns) {
-        FSM.state = ns;
-        FSM.states[FSM.state].start();
-    },
-    states: {
-        begin: {
-            //state 0
-            start() {
-                //if (!nomusic) $("#music1")[0].play();
-                //$("#music1")[0].volume = 0.4;
-                //$("#music2")[0].pause();
-                music.play("music1")
-                video.play("video1")
-                //$("#video1")[0].play();
-                //$("#video2")[0].pause();
-                //$("#video1").show();
-                //$("#video2").hide();
-                $("#maingame").hide();
-                introTimer = new timer.timer("intro", 300);
-            },
-            test() {
-                if (introTimer.state() === 0) {
-                    introTimer.remove();
-                    FSM.newState("titlescreen");
-                }
-            }
-        },
-        titlescreen: {
-            start() {
-                $("#introscreen").show();
-            },
-            test() {
-                if (keyboard.wasEnterPressed()) {
-                    FSM.newState("intro0");
-                }
-            }
-        },
-        intro0: {
-            done: false,
-            async start() {
-                //console.log(this)
-
-                $("#introscreen").hide();
-                $("#maingame").show();
-                keyboard.doOutput(true);
-                var intro0 = require("../game/text/introText.js")
-                var intro1 = require("../game/text/introText1.js")
-                var n = await display.printTextMultiline(intro0, true)
-                display.cls()
-                music.play("openscore")
-                video.play("openscore")
-                n = await display.printTextMultiline(intro1, true)
-                //console.log("DONEPRINT")
-                this.done = true
-                keyboard.wasEnterPressed()
-            },
-            test() {
-                if (this.done) {
-                    display.cls();
-                    FSM.newState("game0");
-                }
-            }
-        },
-        game0: {
-            start() {
-                $("#introscreen").hide();
-                $("#maingame").show();
-                //if (!nomusic) $("#music2")[0].play();
-                //$("#music2")[0].volume = 0.4
-                //$("#music1")[0].pause();
-                //$("#music2").show();
-                //$("#music1").hide();
-                //music.play("music2")
-                //$("#video2").show();
-                //$("#video1").hide();
-                //$("#video2")[0].play();
-                //$("#video1")[0].pause();
-                //music.play("music2")
-                //video.play("video2")
-                keyboard.doOutput(true);
-                game.cEnter();
-                keyboard.key(0);
-            },
-            test() {}
+  state: "begin",
+  newState(ns) {
+    FSM.state = ns;
+    FSM.states[FSM.state].start();
+  },
+  states: {
+    begin: {
+      //state 0
+      start() {
+        //if (!nomusic) $("#music1")[0].play();
+        //$("#music1")[0].volume = 0.4;
+        //$("#music2")[0].pause();
+        music.play("music1");
+        video.play("video1");
+        //$("#video1")[0].play();
+        //$("#video2")[0].pause();
+        //$("#video1").show();
+        //$("#video2").hide();
+        $("#maingame").hide();
+        introTimer = new timer.timer("intro", 300);
+      },
+      test() {
+        if (introTimer.state() === 0) {
+          introTimer.remove();
+          FSM.newState("titlescreen");
         }
+      }
+    },
+    titlescreen: {
+      start() {
+        $("#introscreen").show();
+      },
+      test() {
+        if (keyboard.wasEnterPressed()) {
+          FSM.newState("intro0");
+        }
+      }
+    },
+    intro0: {
+      done: false,
+      async start() {
+        //console.log(this)
+
+        $("#introscreen").hide();
+        $("#maingame").show();
+        keyboard.doOutput(true);
+        var intro0 = require("../game/text/introText.js");
+        var intro1 = require("../game/text/introText1.js");
+        var n = await display.printTextMultiline(intro0, true);
+        display.cls();
+        n = await music.fade();
+        music.play("openscore");
+        video.play("openscore");
+        n = await display.printTextMultiline(intro1, true);
+        //console.log("DONEPRINT")
+        this.done = true;
+        keyboard.wasEnterPressed();
+      },
+      test() {
+        if (this.done) {
+          display.cls();
+          FSM.newState("game0");
+        }
+      }
+    },
+    game0: {
+      start() {
+        $("#introscreen").hide();
+        $("#maingame").show();
+        //if (!nomusic) $("#music2")[0].play();
+        //$("#music2")[0].volume = 0.4
+        //$("#music1")[0].pause();
+        //$("#music2").show();
+        //$("#music1").hide();
+        //music.play("music2")
+        //$("#video2").show();
+        //$("#video1").hide();
+        //$("#video2")[0].play();
+        //$("#video1")[0].pause();
+        //music.play("music2")
+        //video.play("video2")
+        keyboard.doOutput(true);
+        game.cEnter();
+        keyboard.key(0);
+      },
+      test() {}
     }
+  }
 };
 
-module.exports = FSM
+module.exports = FSM;
--- js/music.js
+++ js/music.js
@@ -1,27 +1,48 @@
 var mute = false;
 var played = null;
-const nomusic = true;
+const nomusic = !true;
 
-var stop = (id) => {
-    $("audio#" + id)[0].pause();
-    $("audio#" + id).hide();
+var stop = id => {
+  $("audio#" + id)[0].pause();
+  $("audio#" + id).hide();
 
-    played = null;
-}
+  played = null;
+};
 
+var wait = async ms => {
+  return new Promise(resolve => {
+    setTimeout(resolve, ms);
+  });
+};
 
-var play = (id) => {
-    if (played) stop(played);
-    if (!nomusic) $("audio#" + id)[0].play();
-    $("audio#" + id)[0].volume = 0.4;
-    $("audio#" + id)[0].onended = function () {
-        alert("The audio has ended" + id);
-    };
-    $("audio#" + id).show();
-    played = id;
-}
+var fade = async () => {
+  var audio = $("audio#" + played)[0];
+
+  var newvol = audio.volume - 0.1;
+  while (newvol > 0) {
+    audio.volume = newvol;
+    newvol -= 0.1;
+    await wait(400);
+  }
+  audio.pause();
+  $("audio#" + played).hide();
+  played = null;
+  return;
+};
+
+var play = id => {
+  if (played) stop(played);
+  if (!nomusic) $("audio#" + id)[0].play();
+  $("audio#" + id)[0].volume = 0.5;
+  $("audio#" + id)[0].onended = function() {
+    alert("The audio has ended" + id);
+  };
+  $("audio#" + id).show();
+  played = id;
+};
 
 module.exports = {
-    play,
-    stop
-}
+  play,
+  stop,
+  fade
+};
