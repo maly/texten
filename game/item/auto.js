f06ef0c3da7fc5c6d92230dfbe2452a94aa8d628
Martin Maly <maly@maly.cz>
2019-07-30 14:24:21 +0200

    Předměty, koncept bedny

--- game/item/auto.js
+++ game/item/auto.js
@@ -2,8 +2,9 @@ var item = {
     id: "auto",
     name: "aut-o,a,u",
     desc: "Dodge Grand Caravan. Takové slušné SUV, sedm míst, hodně prostoru pro spoustu krámů...",
-    attrs: ["nonmovable"],
+    attrs: ["nonmovable", "crate"],
     where: "road_r1",
+
     handlers: []
 };
 
