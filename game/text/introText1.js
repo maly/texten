4c5780972db08b10d109049657114ebe46a80872
Martin Maly <maly@maly.cz>
2019-07-30 12:45:42 +0200

    Texts

---
+++ game/text/introText1.js
@@ -0,0 +1,9 @@
+var bigText = `Na druhou stranu by se našlo pár věcí, co chlap v tvojí situaci nepotřebuje.
+
+Nepotřebuje policajty za prdelí.
+Nepotřebuje ženskou, co by mu kecala do řízení.
+A už vůbec nepotřebuje píchnout kolo v situaci, kdy má hlad, je špinavej, po celým dni utahanej, navíc trošku začíná pršet, do nejbližšího města je bůhví kolik desítek mil a taky se stmívá...
+`
+
+
+module.exports = bigText;
