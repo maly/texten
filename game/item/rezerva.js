cb9bfb1304a00892808cd9456923839d66f45491
Martin Maly <maly@maly.cz>
2019-07-30 18:04:35 +0200

    minifix

--- game/item/rezerva.js
+++ game/item/rezerva.js
@@ -1,11 +1,11 @@
 var item = {
-    id: "rezerva",
-    name: "rezerv-a,y,ě,u",
-    desc: "Rezervní kolo. Ještě že ho máš s sebou! Jen škoda, že sis tehdy, když žena píchla, řekl, že to opravíš až pak, někdy, a pak ses na to vykašlal.",
-    attrs: [],
-    where: "auto",
-
-    handlers: []
+  id: "rezerva",
+  name: "rezerv-a,y,ě,u",
+  desc:
+    "Rezervní kolo. Ještě že ho máš s sebou! Jen škoda, že sis tehdy, když žena píchla, řekl, že to opravíš až pak, někdy, a pak ses na to vykašlal.",
+  attrs: [],
+  where: "auto",
+  handlers: []
 };
 
 module.exports = item;
