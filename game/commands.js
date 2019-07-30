8ec1cbb6df3f9c15810b4ea4d8c93ecbd90978df
Martin Maly <maly@maly.cz>
2019-07-30 18:05:08 +0200

    cExamIn fixed

--- game/commands.js
+++ game/commands.js
@@ -6,7 +6,8 @@ $ - item carry
 # - here or carry
 */
 
-const commands = [{
+const commands = [
+  {
     id: "cgo",
     _cmd: ["jdi ^", "běž ^", "utíkej ^"],
     _does: 'print "Jdeš ^."; exit ^'
@@ -20,7 +21,8 @@ const commands = [{
     id: "ctake",
     //cmd: [["zvedni","%"],["seber","%"]],
     _cmd: ["zvedni %3", "seber %3", "vezmi %3"],
-    _does: 'C $: . "Vždyť máš $ u sebe!";B ###' +
+    _does:
+      'C $: . "Vždyť máš $ u sebe!";B ###' +
       'NH $:. "V téhle místnosti nevidíš nic takového!";B###' +
       'AS $ nonmovable: . "Nemůžeš zvednout $";B###' +
       'AR $ nonmovable: P $; . "Zvedl jsi $."'
@@ -39,27 +41,13 @@ const commands = [{
     id: "cuseon",
     _cmd: ["použij #3 na #3"],
     does: [
-      [
-        [],
-        [
-          [".", "Použil jsi $ na #"],
-          ["UON", "$", "#", "Nic se nestalo"]
-        ]
-      ]
+      [[], [[".", "Použil jsi $ na #"], ["UON", "$", "#", "Nic se nestalo"]]]
     ]
   },
   {
     id: "cuse",
     _cmd: ["použij #3"],
-    does: [
-      [
-        [],
-        [
-          [".", "Použil jsi $"],
-          ["U", "$", "Nic se nestalo"]
-        ]
-      ]
-    ]
+    does: [[[], [[".", "Použil jsi $"], ["U", "$", "Nic se nestalo"]]]]
   },
 
   {
@@ -68,9 +56,13 @@ const commands = [{
     _prerun(g) {},
     _postrun(g) {},
     _run(p, g) {
-      console.log(p)
-      p = g.sysDecrate(p)
-      g.sysExamine(p)
+      console.log("cExamIn", this, p);
+      p = g.sysDecrate(p);
+      if (!p || p.length === 0) {
+        g.err("Jsi si jist, že to tam je?");
+      } else {
+        g.sysExamine(p);
+      }
     },
     _does: '. "Prohlížíš si $."; PD $; IH $ examine'
   },
@@ -80,7 +72,15 @@ const commands = [{
     _prerun(g) {},
     _postrun(g) {},
     _run(p, g) {
-      g.sysExamine(p)
+      g.sysExamine(p);
+    },
+    _noparam: {
+      type: "shuffle",
+      strings: [
+        "Nevím přesně co zkoumat.",
+        "Asi nechápu, co chceš zkoumat.",
+        "Nerozumím. Co chceš vykoumat?"
+      ]
     },
     _does: '. "Prohlížíš si $."; PD $; IH $ examine'
   }
