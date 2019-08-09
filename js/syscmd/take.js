01b571270df159675d5ad1629316a0aa68cedbae
Martin Maly <maly@maly.cz>
2019-08-09 19:51:20 +0200

    CTake - když to nejde

--- js/syscmd/take.js
+++ js/syscmd/take.js
@@ -1,8 +1,15 @@
 var display = require("../display.js");
+var lang = require("../language.js");
 
-module.exports = async (game, pars) => {
+module.exports = async (game, pars, cmd) => {
   var itm = game.getItem(pars[0]);
   console.log("Take", itm, pars);
+  if (!itm.isMovable()) {
+    if (cmd._nonmovable) {
+      game.err(lang.formatItem(cmd._nonmovable, itm));
+      return;
+    }
+  }
   display.printTextYellow("Vzal jsi " + game.itemFullName(itm)[3] + ".");
   game.takeItem(itm.id);
 };
