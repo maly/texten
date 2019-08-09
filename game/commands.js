9d7f1bd244f7a6dc95627b87e062206da3e4241c
Martin Maly <maly@maly.cz>
2019-08-09 18:51:07 +0200

    cmds remove unnecessary

--- game/commands.js
+++ game/commands.js
@@ -10,9 +10,6 @@ const commands = [
   {
     id: "cgo",
     _cmd: ["jdi ^", "běž ^", "utíkej ^", "j ^"],
-    _does: 'print "Jdeš ^."; exit ^',
-    //_prerun(g) {},
-    //_postrun(g) {},
     _run(p, g) {
       g.sysGo(p);
     },
@@ -22,9 +19,6 @@ const commands = [
   {
     id: "citinerary",
     _cmd: ["i"],
-    _does: 'PI "Máš u sebe " "."',
-    _prerun(g) {},
-    _postrun(g) {},
     _run(p, g) {
       g.sysItinerary(p);
     }
@@ -32,79 +26,77 @@ const commands = [
   {
     id: "croom",
     _cmd: ["rozhlédni", "r"],
-    _does: 'PI "Máš u sebe " "."',
-    _prerun(g) {},
-    _postrun(g) {},
     _run(p, g) {
       g.sysRoomLook(p);
     }
   },
   {
     id: "ctake",
-    //cmd: [["zvedni","%"],["seber","%"]],
     _cmd: ["zvedni %3", "seber %3", "vezmi %3"],
+    /*
     _does:
       'C $: . "Vždyť máš $ u sebe!";B ###' +
       'NH $:. "V téhle místnosti nevidíš nic takového!";B###' +
       'AS $ nonmovable: . "Nemůžeš zvednout $";B###' +
       'AR $ nonmovable: P $; . "Zvedl jsi $."',
+      */
     _run(p, g) {
       g.sysTake(p);
     }
   },
   {
     id: "cinsert",
-    _cmd: ["dej $3 do (crate1)"],
-    _does: 'NC $: . "Nemůžeš položit $!";B###' + 'I $ #; . "Dal jsi $ do #G"'
+    _cmd: ["dej $3 do (crate1)"]
+    //_does: 'NC $: . "Nemůžeš položit $!";B###' + 'I $ #; . "Dal jsi $ do #G"'
   },
   {
     id: "cdrop",
     _cmd: ["polož $3"],
-    _does: 'D $;. "Položil jsi $"',
+    //_does: 'D $;. "Položil jsi $"',
     _run(p, g) {
       g.sysDrop(p);
     }
   },
   {
     id: "cuseon",
-    _cmd: ["použij #3 na #3"],
+    _cmd: ["použij #3 na #3"]
+    /*
     does: [
       [[], [[".", "Použil jsi $ na #"], ["UON", "$", "#", "Nic se nestalo"]]]
     ]
+    */
   },
   {
     id: "cuse",
-    _cmd: ["použij #3"],
-    does: [[[], [[".", "Použil jsi $"], ["U", "$", "Nic se nestalo"]]]]
+    _cmd: ["použij #3"]
+    //does: [[[], [[".", "Použil jsi $"], ["U", "$", "Nic se nestalo"]]]]
   },
 
   {
     id: "cexamin",
     _cmd: ["prozkoumej &3 v #4"],
-    _prerun(g) {},
-    _postrun(g) {},
+    //_prerun(g) {},
+    //_postrun(g) {},
     _run(p, g) {
-      console.log("cExamIn", this, p);
+      //console.log("cExamIn", this, p);
       p = g.sysDecrate(p);
       if (!p || p.length === 0) {
         g.err("Jsi si jist, že to tam je?");
       } else {
         g.sysExamine(p);
       }
-    },
-    _does: '. "Prohlížíš si $."; PD $; IH $ examine'
+    }
   },
   {
     id: "cexam",
     _cmd: ["prozkoumej #3", "prohlédni #"],
-    _prerun(g) {},
-    _postrun(g) {},
+    //_prerun(g) {},
+    //_postrun(g) {},
     _run(p, g) {
       g.sysExamine(p);
     },
     _noparam:
-      "[S[Nevím přesně co zkoumat.][Asi nechápu, co chceš zkoumat.][Nerozumím. Co chceš vykoumat?]]",
-    _does: '. "Prohlížíš si $."; PD $; IH $ examine'
+      "[S[Nevím přesně co zkoumat.][Asi nechápu, co chceš zkoumat.][Nerozumím. Co chceš vykoumat?]]"
   }
 ];
 
