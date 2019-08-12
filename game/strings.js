453050cf82ba684abddf2d636d9c1ace9cea23bd
Martin Maly <maly@maly.cz>
2019-08-12 14:45:41 +0200

    Help text

--- game/strings.js
+++ game/strings.js
@@ -9,6 +9,15 @@ module.exports = {
   NOGO: "Nemůžeš jít nikam.",
   CANGO: "Můžeš jít ",
   HAVE: "Máš u sebe ",
-  SEE: "Vidíš "
+  SEE: "Vidíš ",
   //BEGIN
+  HELP: `"Gordonova farma" je textová hra. Pokud jste se nikdy předtím s textovou hrou nesetkali, pomůže vám pár následujících tipů.
+
+Ve hře ovládáte svou postavu, podobně jako v jiných hrách, Na rozdíl od klasických her v textové hře neovládáte postavu joystickem, ale příkazy, kterými říkáte postavě, co má udělat. Například JDI DO DOMU, ZVEDNI KLÍČ nebo POUŽIJ ZAPALOVAČ. Hra nepoužívá grafiku, místo toho vám popisuje vše důležité, co se děje, rovněž textem. Jedná se vlastně o interaktivní povídku, ovšem její výsledek je zcela na vás.
+
+Příkazy zadáváte nejčastěji pomocí sloves a podstatných jmen. Nejpoužívanější příkazy jsou JDI, VEZMI, POLOŽ, POUŽIJ a PROZKOUMEJ, ale hra rozumí i jiným. Místo VEZMI můžete napsat ZVEDNI nebo SEBER a tak dál. Abyste nemuseli tolik psát, tak stačí místo slovesa zadat jen první tři znaky. U většiny předmětů stačí rovněž první tři znaky. Pokud hře nebude jasné, jaký předmět přesně myslíte, zeptá se na upřesnění.
+
+Jsou i některé speciální příkazy. INVENTÁŘ (nebo zkráceně jen "I") vám vyjmenuje předměty, jaké má postava u sebe (asi v kapsách nebo v rukách). Příkaz ROZHLÉDNI SE (nebo zkráceně R) zopakuje popis místa, v němž se nachází vaše postava. Pomocí SAVE se uloží stav hry do paměti v prohlížeči, pomocí LOAD ho zase nahrajete zpět. Můžete si uložit až 9 různých her (SAVE 1, SAVE 2, ... SAVE 9) a u příkazu LOAD zadat číslo pozice.
+
+Na zbytek si ale musíte přijít sami. Jen prozradím, že hra má několik možných konců a každé vaše rozhodnutí může hrát zásadní roli...`
 };
--- game/text/introText1.js
+++ game/text/introText1.js
@@ -3,7 +3,8 @@ var bigText = `Na druhou stranu by se našlo pár věcí, co chlap v tvojí situ
 Nepotřebuje policajty za prdelí.
 Nepotřebuje ženskou, co by mu kecala do řízení.
 A už vůbec nepotřebuje píchnout kolo v situaci, kdy má hlad, je špinavej, po celým dni utahanej, navíc trošku začíná pršet, do nejbližšího města je bůhví kolik desítek mil a taky se stmívá...
-`
+
+(Hodně štěstí! Od této chvíle je to na vás. Když nebudete vědět, napište POMOC!)`
 
 
 module.exports = bigText;
